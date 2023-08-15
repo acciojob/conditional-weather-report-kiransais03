b265b164b134ffd55e0ea767d8ebcdb4f37d7be5
kiransai <kiransais03@gmail.com>
2023-08-15 14:20:11 +0530

    submitting code-Tue Aug 15 2023 14:20:04 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [input,setInput] = useState({ temperature: 25, conditions: "Sunny" });
+  if(input)
   return (
     <div>
         {/* Do not remove the main div */}
+       <input type="number" value={input.temperature} onChange={(e)=>{
+        if(e.target.value>20)
+        {
+          setInput({temperature:e.target.value,conditions: "Sunny"});
+        }
+          else
+          {
+            setInput({temperature: e.target.value, conditions: "Cold"})
+          }}}/>
+       {input && <p className={`${input.conditions}`}>The temperature is {input.temperature} degrees.Weather Condition is {input.conditions}</p>}
     </div>
   )
 }
